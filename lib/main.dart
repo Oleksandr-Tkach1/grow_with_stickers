538e129790f663e4e877bed8c60d159722ecff85
Mykola Popov <popovnikolay.dev@gmail.com>
2024-02-12 21:28:49 +0200

    setup firebase in project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -10,38 +11,41 @@ import 'bloc/main_bloc/main_state.dart';
 import 'data/SQLite/SQLite_service.dart';
 import 'data/SQLite/utils/model_mapper.dart';
 import 'network/services/firebase/firebase_database.dart';
+import 'firebase_options.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   final SqliteService _sqliteService = SqliteService.instance;
   _sqliteService.database;
   final FirebaseDatabase _firebaseDatabase = FirebaseDatabase();
   final ModelMapper modelMapper = ModelMapper();
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [SystemUiOverlay.top]);
-  runApp(
-      MultiRepositoryProvider(
-          providers: getRepositories(_sqliteService, _firebaseDatabase),
-          child: MultiBlocProvider(providers: getProviders(_sqliteService, modelMapper), child: const MyApp())
-      )
-  );
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      overlays: [SystemUiOverlay.top]);
+  runApp(MultiRepositoryProvider(
+      providers: getRepositories(_sqliteService, _firebaseDatabase),
+      child: MultiBlocProvider(
+          providers: getProviders(_sqliteService, modelMapper),
+          child: const MyApp())));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<MainBloc, MainState>(
-        builder: (context, state) {
-          return MaterialApp(
-            title: 'Grow with stickers',
-            theme: ThemeData(
-              focusColor: Colors.grey,
-              useMaterial3: true,
-            ),
-            home: navigateToHomeWidget(state),
-          );
-        }
-    );
+    return BlocBuilder<MainBloc, MainState>(builder: (context, state) {
+      return MaterialApp(
+        title: 'Grow with stickers',
+        theme: ThemeData(
+          focusColor: Colors.grey,
+          useMaterial3: true,
+        ),
+        home: navigateToHomeWidget(state),
+      );
+    });
   }
 
   Widget navigateToHomeWidget(MainState state) {
@@ -52,7 +56,8 @@ class MyApp extends StatelessWidget {
         return const HomeScreen();
       case AuthenticationStatus.unauthenticated:
         return const StartScreen();
-      default: return const StartScreen();
+      default:
+        return const StartScreen();
     }
   }
 }
